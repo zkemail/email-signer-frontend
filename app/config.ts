71adbb0a322cd6de2ddbf751248c6560eea78cd4
John Guilding <jwguilding@gmail.com>
2025-02-25 17:35:46 -0300

    feat: get or deploy email signer

---
+++ app/config.ts
@@ -0,0 +1,37 @@
+import { isHex } from "viem";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.NEXT_PUBLIC_DEPLOYER_PRIVATE_KEY) {
+  throw new Error("DEPLOYER_PRIVATE_KEY environment variable is required");
+}
+if (!isHex(process.env.NEXT_PUBLIC_DEPLOYER_PRIVATE_KEY)) {
+  throw new Error("NEXT_PUBLIC_DEPLOYER_PRIVATE_KEY does not start with 0x");
+}
+if (!process.env.NEXT_PUBLIC_RPC_URL) {
+  throw new Error("RPC_URL environment variable is required");
+}
+if (!process.env.NEXT_PUBLIC_RELAYER_URL) {
+  throw new Error("RELAYER_URL environment variable is required");
+}
+if (!process.env.NEXT_PUBLIC_EMAIL_SIGNER_FACTORY_ADDRESS) {
+  throw new Error(
+    "EMAIL_SIGNER_FACTORY_ADDRESS environment variable is required"
+  );
+}
+if (!isHex(process.env.NEXT_PUBLIC_EMAIL_SIGNER_FACTORY_ADDRESS)) {
+  throw new Error("EMAIL_SIGNER_FACTORY_ADDRESS does not start with 0x");
+}
+
+const DEPLOYER_PRIVATE_KEY = process.env.NEXT_PUBLIC_DEPLOYER_PRIVATE_KEY;
+const RPC_URL = process.env.NEXT_PUBLIC_RPC_URL;
+const RELAYER_URL = process.env.NEXT_PUBLIC_RELAYER_URL;
+const EMAIL_SIGNER_FACTORY_ADDRESS =
+  process.env.NEXT_PUBLIC_EMAIL_SIGNER_FACTORY_ADDRESS;
+
+export {
+  RPC_URL,
+  RELAYER_URL,
+  EMAIL_SIGNER_FACTORY_ADDRESS,
+  DEPLOYER_PRIVATE_KEY,
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,110 @@
-import Image from "next/image";
+"use client";
+import {
+  bytesToHex,
+  createPublicClient,
+  createWalletClient,
+  http,
+  parseAbi,
+} from "viem";
+import {
+  DEPLOYER_PRIVATE_KEY,
+  EMAIL_SIGNER_FACTORY_ADDRESS,
+  RELAYER_URL,
+  RPC_URL,
+} from "./config";
+import { baseSepolia } from "viem/chains";
+import { useState } from "react";
+import { privateKeyToAccount } from "viem/accounts";
+import { buildPoseidon } from "circomlibjs";
+
+const account = privateKeyToAccount(DEPLOYER_PRIVATE_KEY);
+const publicClient = createPublicClient({
+  chain: baseSepolia,
+  transport: http(RPC_URL),
+});
+const walletClient = createWalletClient({
+  account,
+  chain: baseSepolia,
+  transport: http(RPC_URL),
+});
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [emailSignerAddress, setEmailSignerAddress] = useState("");
+
+  const generateAccountCode = async () => {
+    const poseidon = await buildPoseidon();
+    const accountCodeBytes: Uint8Array = poseidon.F.random();
+    return bytesToHex(accountCodeBytes.reverse());
+  };
+
+  const getOrDeployEmailSigner = async (email: string) => {
+    const accountCode = await generateAccountCode();
+
+    const { accountSalt } = await fetch(`${RELAYER_URL}/api/accountSalt`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        accountCode: accountCode,
+        emailAddress: email,
+      }),
+    }).then((res) => res.json());
+    console.log("accountSalt: ", accountSalt);
+
+    const emailSignerFactoryAbi = parseAbi([
+      "function predictAddress(bytes32 accountSalt) view returns (address)",
+      "function deploy(bytes32 accountSalt) returns (address)",
+    ]);
+
+    const emailSignerAddress = await publicClient.readContract({
+      address: EMAIL_SIGNER_FACTORY_ADDRESS,
+      abi: emailSignerFactoryAbi,
+      functionName: "predictAddress",
+      args: [accountSalt],
+    });
+    console.log("emailSignerAddress: ", emailSignerAddress);
+
+    const bytecode = await publicClient.getCode({
+      address: emailSignerAddress,
+    });
+    const isEmailSignerDeployed = !!bytecode;
+    console.log("Email signer contract deployed:", isEmailSignerDeployed);
+
+    if (!isEmailSignerDeployed) {
+      console.log("Deploying email signer contract...");
+      const deployTxHash = await walletClient.writeContract({
+        address: EMAIL_SIGNER_FACTORY_ADDRESS,
+        abi: emailSignerFactoryAbi,
+        functionName: "deploy",
+        args: [accountSalt],
+      });
+      await publicClient.waitForTransactionReceipt({ hash: deployTxHash });
+      console.log("Email signer contract deployed successfully");
+    } else {
+      console.log("Email signer contract already deployed");
+    }
+
+    setEmailSignerAddress(emailSignerAddress);
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <h1>Email Signer</h1>
+        <p>Email Signer Address: {emailSignerAddress}</p>
+        <form action={() => getOrDeployEmailSigner(email)}>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <button type="submit">Generate email signer</button>
+        </form>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
   );
 }
